aa8ffac5bb188d9d48d72fd0360511a0ef689f8c
Brian Ressler <briancressler@gmail.com>
2024-08-04 21:33:27 -0500

    echo src and size details for images

--- _static/uicontrol.js
+++ _static/uicontrol.js
@@ -4,6 +4,14 @@ function optimize_images() {
     let page_images = document.getElementsByTagName("img");
     console.log("Page Images:");
     console.log(page_images);
+
+    if(page_images != null && page_images.length > 0) {
+        for(let i = 0; i < page_images.length; i++) {
+            let current_image = page_images[i];
+
+            console.log("src: " + current_image.src + ", width: " + current_image.width + ", height: " + current_image.height);
+        }
+    }
 }
 
 optimize_images();
