2fd7836971325c469b9e1ff5881b702468e58fde
bressler95tops <bressler95tops@users.noreply.github.com>
2024-08-05 03:04:44 +0000

    deploy: ed2fb22e85113f6f18fa49f0b6b5a0133f883215

--- _static/uicontrol.js
+++ _static/uicontrol.js
@@ -1,22 +1,23 @@
 
-
-function optimize_images() {
-    let page_images = document.getElementsByTagName("img");
-    console.log("Page Images:");
-
-    let image_arr = Array.prototype.slice.call(page_images)
-
-    for (let image of image_arr) {
-        console.log(image);
+document.addEventListener("DOMContentLoaded", function() {
+    function optimize_images() {
+        let page_images = document.getElementsByTagName("img");
+        console.log("Page Images:");
+    
+        let image_arr = Array.prototype.slice.call(page_images)
+    
+        for (let image of image_arr) {
+            console.log(image);
+        }
+    
+        // if(page_images != null && page_images.length > 0) {
+        //     for(let i = 0; i < page_images.length; i++) {
+        //         let current_image = page_images[i];
+    
+        //         console.log("src: " + current_image.src + ", width: " + current_image.width + ", height: " + current_image.height);
+        //     }
+        // }
     }
-
-    // if(page_images != null && page_images.length > 0) {
-    //     for(let i = 0; i < page_images.length; i++) {
-    //         let current_image = page_images[i];
-
-    //         console.log("src: " + current_image.src + ", width: " + current_image.width + ", height: " + current_image.height);
-    //     }
-    // }
-}
-
-optimize_images();
+    
+    optimize_images();
+});
