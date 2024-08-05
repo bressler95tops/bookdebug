0188e56061b2b8cd91138ae228a0b5c79bf9d214
bressler95tops <bressler95tops@users.noreply.github.com>
2024-08-05 02:26:41 +0000

    deploy: dc3f6a93b07281a5b0def270507c3a8db938cde5

---
+++ _static/uicontrol.js
@@ -0,0 +1,9 @@
+
+
+function optimize_images() {
+    let page_images = document.getElementsByTagName("img");
+    console.log("Page Images:");
+    console.log(page_images);
+}
+
+optimize_images();
